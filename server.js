f5b24cbafc68808c299570480f0ad75c258e94a9
Vân Vân Bùi <dieuvanptit@gmail.com>
2019-03-19 17:41:49 +0700

    todo models

--- server.js
+++ server.js
@@ -1,20 +1,49 @@
-/**
- * Created by hoangdv on 0028, Jun, 28, 2017.
- */
-let express = require('express');
-let app = express();
-let morgan = require('morgan');
-let bodyParser = require('body-parser');
-let port = process.env.PORT || 8080;
-let book = require('./routes/book');
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8080;
+const book = require('./app/routes/book');
+const config = require('config');// load db location from json files;
+const mongoose = require('mongoose');
 
 
-if(process.env.NODE_ENV !== 'test') {
-    //use morgan to log at command line
-    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
-}
+// db option
+const options = { 
+    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } 
+  }; 
+var uri = "mongodb://localhost:27017/StoreBook"
+mongoose.connect('mongodb://localhost:27017/StoreBook', {useNewUrlParser: true});
 
-//parse application/json and look for raw text
+mongoose.connect(uri, { keepAlive: true, keepAliveInitialDelay: 300000 }, function(err){
+             console.log('Mongoose disconnected!!');
+});
+
+// )
+
+
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+
+// //don't show the log when it is test
+// if(config.util.getEnv('NODE_ENV') !== 'test') {
+// 	//use morgan to log at command line
+// 	app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+// }
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI, err => {
+//     if(err) 
+//         console.log(err);
+//     } 
+// );
+
+
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
@@ -22,10 +51,10 @@ app.use(bodyParser.json({ type: 'application/json'}));
 
 app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
 
-app.route("/books")
+app.route("/book")
     .get(book.getBooks)
     .post(book.postBook);
-app.route("/books/:id")
+app.route("/book/:id")
     .get(book.getBook)
     .delete(book.deleteBook)
     .put(book.updateBook);
@@ -33,4 +62,4 @@ app.route("/books/:id")
 app.listen(port);
 console.log("Listening on port " + port);
 
-module.exports = app;
+module.exports = app;// for testing
--- test/book.js
+++ test/book.js
@@ -1,6 +1,7 @@
-
 process.env.NODE_ENV = 'test';
 
+let mongoose = require("mongoose");
+let Book = require('../app/models/book');
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
@@ -8,134 +9,122 @@ let server = require('../server');
 let should = chai.should();
 
 chai.use(chaiHttp);
-//Our parent block
+
 describe('Books', () => {
     beforeEach((done) => {
-        //Before each test we empty the database in your case
-        done();
+        Book.remove({}, (err) => { 
+           done();         
+        });     
     });
-    /*
-     * Test the /GET route
-     */
-    describe('/GET books', () => {
-        it('it should GET all the books', (done) => {
+  describe('/GET book', () => {
+      it('it should GET all the books', (done) => {
             chai.request(server)
-                .get('/books')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('array');
-                    // res.body.length.should.be.eql(100); 
-                    done();
-                });
+            .get('/book')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+  describe('/POST book', () => {
+      it('it should not POST a book without pages field', (done) => {
+        let book = {
+            title: "The Lord of the Rings",
+            author: "J.R.R. Tolkien",
+            year: 1954
+        }
+            chai.request(server)
+            .post('/book')
+            .send(book)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('errors');
+                res.body.errors.should.have.property('pages');
+                res.body.errors.pages.should.have.property('kind').eql('required');
+              done();
+            });
+      });
+      it('it should POST a book ', (done) => {
+        let book = {
+            title: "The Lord of the Rings",
+            author: "J.R.R. Tolkien",
+            year: 1954,
+            pages: 1170
+        }
+            chai.request(server)
+            .post('/book')
+            .send(book)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql('Book successfully added!');
+                res.body.book.should.have.property('title');
+                res.body.book.should.have.property('author');
+                res.body.book.should.have.property('pages');
+                res.body.book.should.have.property('year');
+              done();
+            });
+      });
+  });
+  describe('/GET/:id book', () => {
+      it('it should GET a book by the given id', (done) => {
+        let book = new Book({ title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954, pages: 1170 });
+        book.save((err, book) => {
+            chai.request(server)
+            .get('/book/' + book.id)
+            .send(book)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('title');
+                res.body.should.have.property('author');
+                res.body.should.have.property('pages');
+                res.body.should.have.property('year');
+                res.body.should.have.property('_id').eql(book.id);
+              done();
+            });
         });
-    });
 
-    /*
-     * Test the /POST route
-     */
-    describe('/POST books', () => {
-        it('it should POST a book', (done) => {
-            let book = {
-                name: "Bug",
-                status: "detected"
-            };
-            chai.request(server)
-                .post('/books')
-                .send(book)
+      });
+  });
+  describe('/PUT/:id book', () => {
+      it('it should UPDATE a book given the id', (done) => {
+        let book = new Book({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
+        book.save((err, book) => {
+                chai.request(server)
+                .put('/book/' + book.id)
+                .send({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1950, pages: 778})
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('message').eql('Book successfully added!');
-                    res.body.book.should.have.property('id');
-                    res.body.book.should.have.property('name').eql(book.name);
-                    res.body.book.should.have.property('status').eql(book.status);
-                    done();
+                    res.body.should.have.property('message').eql('Book updated!');
+                    res.body.book.should.have.property('year').eql(1950);
+                  done();
                 });
-        });
-        it('it should not POST a book without status field', (done) => {
-            let book = {
-                name: "Bug"
-            };
-            chai.request(server)
-                .post('/books')
-                .send(book)
+          });
+      });
+  });
+ /*
+  * Test the /DELETE/:id route
+  */
+  describe('/DELETE/:id book', () => {
+      it('it should DELETE a book given the id', (done) => {
+        let book = new Book({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
+        book.save((err, book) => {
+                chai.request(server)
+                .delete('/book/' + book.id)
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('message').eql("Book is invalid!");
-                    done();
-                });
-        });
-    });
-
-    /*
-     * Test the /GET/:id route
-     */
-    describe('/GET/:id books', () => {
-        it('it should GET a book by the given id', (done) => {
-            // TODO add a model to db then get that id to take this test
-            let id = 1;
-            chai.request(server)
-                .get('/books/' + id)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('book');
-                    res.body.book.should.have.property('id').eql(id);
-                    res.body.book.should.have.property('name');
-                    res.body.book.should.have.property('status');
-                    done();
-                });
-        });
-    });
-
-    /*
-     * Test the /PUT/:id route
-     */
-    describe('/PUT/:id books', () => {
-        it('it should UPDATE a book given the id', (done) => {
-            // TODO add a model to db then get that id to take this test
-            let id = 1;
-            chai.request(server)
-                .put('/books/' + id)
-                .send({
-                    name: "Bug",
-                    status: "fixed"
-                })
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('book');
-                    res.body.book.should.have.property('name').eql("Bug");
-                    res.body.book.should.have.property('status').eql("fixed");
-                    done();
-                });
-        });
-    });
-
-    /*
-     * Test the /DELETE/:id route
-     */
-    describe('/DELETE/:id books', () => {
-        it('it should DELETE a book given the id', (done) => {
-            // TODO add a model to db then get that id to take this test
-            let id = 1;
-            chai.request(server)
-                .delete('/books/' + id)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    
-                    
-                    res.body.should.be.a('object');
-                    // res.body.should.have.property('book');
-                    // res.body.book.should.be.a('object');
-                    // res.body.book.should.have.property('id').eql(id);
                     res.body.should.have.property('message').eql('Book successfully deleted!');
-                    res.body.should.have.property('result');
-                    res.body.result.should.have.property('remoBook').eql(1);
-                    done();
+                    res.body.result.should.have.property('ok').eql(1);
+                    res.body.result.should.have.property('n').eql(1);
+                  done();
                 });
-        });
-    });
+          });
+      });
+  });
 });
-
